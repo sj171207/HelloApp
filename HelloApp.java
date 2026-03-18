4ade97fc8c7ad44838e9fa0bf23b245ee9703522
sj171207 <ss8983@srmist.edu.in>
2026-03-18 10:40:35 +0530

    Hello app uc4

--- HelloApp.java
+++ HelloApp.java
@@ -1,12 +1,27 @@
 public class Helloapp{
 
-    public static void main(String[] args){
-         String name = "World";
-         if (args.length > 0){
-            name = args[0];
-         }
-         System.out.println("Hello, "+ name + " !");
-    }
-    
+    public static void main(String[] args) {
 
+        String name;
+
+        if (args.length > 0) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
+
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
 }
